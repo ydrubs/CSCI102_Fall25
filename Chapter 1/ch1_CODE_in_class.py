5716adb034802407b51d6a63ac79935eed1a46a1
ydrubs <ydrubins@yahoo.com>
2024-12-17 16:21:59 -0600

    initial commit

---
+++ Chapter 1/ch1_CODE_in_class.py
@@ -0,0 +1,66 @@
+###Slide 3
+
+
+###Slide 13 - Data types
+
+
+
+###Slide 15 - Getting Input
+
+
+
+##Slide 16 Activity
+
+
+####Slide 17 Recasting Input
+
+
+
+
+###Slide 20 Outputting multiple data
+# name = input("Enter your name: ")
+# print() #Using commas to seperate data
+# print() # Using + to concatenate data into one string
+# print() # Using f-strings
+
+###Slide 21 Operations
+# a =
+# b =
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+
+##Slide 25
+##Run-Time error - Program stops execution when it encounters an error
+# length = int(input("Enter the length: "))
+# print(legnth)
+
+
+##Syntax Error - Program does not execute at all
+# length = int(input("Enter the length: "))
+# print{legnth}
+
+
+##Sementic Error
+# length = int(input("Enter the length: "))
+# print("The area of the square is ", 2*length)
+
+
+##Slide 26
+ # print("Hello")
+
+
+# a = 2
+# b = 3
+# print(a +)
+
+
+##Slide 27
+##Traceback
+# def say(name):
+#     print('Hello, ' + nam)
+#
+# say('Michael')
