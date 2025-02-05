47163714fbd374e23d8b382660fccb18372bd7c0
yuriy.drubinskiy <ydrubins@yahoo.com>
2025-02-05 08:01:59 -0600

    initial commit

---
+++ Chapter 2/Exercises/Exercise 2.1 - Performing Operations.py
@@ -0,0 +1,37 @@
+"""
+Please complete BOTH exercises (described below)
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+##
+
+
+"""
+Exercise 2.1a - Spheres
+
+
+Write a program that takes the radius of a sphere (a floating-point number) as input
+
+and then outputs the sphere’s diameter, circumference, surface area, and volume (feel free to google the formulas).
+
+"""
+# YOUR CODE HERE
+
+
+
+
+
+"""
+Exercise 2.1b - Hourly Wage Calculator
+
+
+An employee’s total weekly pay equals the hourly wage multiplied by the total number of regular hours
+
+plus any overtime pay. Overtime pay equals the total overtime hours multiplied by 1.5 times the hourly wage.
+
+Write a program that takes as inputs the hourly wage, total regular hours, and total overtime hours
+
+and displays an employee’s total weekly pay. All values should be floats.
+"""
+# YOUR CODE HERE
