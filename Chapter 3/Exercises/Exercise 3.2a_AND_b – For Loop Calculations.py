44ea74ec991b712b48429e7eef00a50e227ec097
ydrubs <ydrubins@yahoo.com>
2025-03-03 08:15:11 -0600

    initial commit

---
+++ Chapter 3/Exercises/Exercise 3.2a_AND_b – For Loop Calculations.py
@@ -0,0 +1,50 @@
+"""
+Please Complete BOTH exercises
+    3.2A
+    3.2B
+"""
+
+
+"""
+Exercise 3.2a – Pyramid Number Problem
+
+A pyramidal number has the form 1^2+2^2+3^2+.......
+
+for example the forth pyramidal number is 30 because:
+    1^2+2^2+3^2^4^2 = 1+4+9+16=30
+
+It is also known as the cannonball stacking number becasue it represents how much spherical cannonballs
+can be stacked in a pyramid shape with a given number of rows.
+
+Your program should:
+    1) Ask the user how many rows of cannonballs they want to stack
+    2) Calculate the total number of cannonballs that will create the pyramid with the specified rows
+    
+    For instance if the user says 4 - the return is 30 as described above. 
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+###YOUR SOLUTION HERE
+
+
+
+
+
+""""
+Exercise 3.2b - Sum of a sequence
+
+The program asks the user for input and calculates the sum of sequence based on the following - 
+
+    1. Ask the user for the first number in a sequence as a single integer digit.
+    2. Ask the user for the number of terms in a sequence. 
+    
+    Calculate the sum of the sequence - for example:
+        Starting digit is 3
+        Number of terms is 5
+        
+        The program will calculate: 3+33+333+3333+33333
+            ... The program returns 37035 as the answer
+    HINT: Think about what you learned in chapter 2 about multiplying strings as well as when to recast to an int. 
+"""
+###YOUR SOLUTION HERE
