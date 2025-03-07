0c52c6640ad4b56aaed0602537cb65cb3b145775
ydrubs <ydrubins@yahoo.com>
2025-03-07 10:38:36 -0600

    initial commit

---
+++ Chapter 3/Exercises/Exercise 3.3 - Simple Password Validation.py
@@ -0,0 +1,9 @@
+"""
+Exercise 3.3 - Simple Password validation
+
+Write a program that continues to ask the user for a password as long as the password is not 'password123'.
+
+There is one caveat -  Every three incorrect entries, the program should ask the user "Are you sure you are not a robot":
+    If the user responds with 'y' they can continue to put in the password.
+    If they respond with 'n' the loop (and program) stops, you can give a funny message if you want.
+"""
