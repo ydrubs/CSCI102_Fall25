6d49aecf997ed0d8ca0ea6a2cf6c633a1db02142
ydrubs <ydrubins@yahoo.com>
2025-03-26 08:05:43 -0500

    initial commit

---
+++ Chapter 4/Exercises/Exercise 4.1 - Only Uppercase.py
@@ -0,0 +1,14 @@
+"""
+Exercise 4.1 - Only Uppercase
+
+Ask the user for an input string.
+
+The program should print a string that only contains the uppercase letters from the string the user entered.
+
+For example:
+    If the user enters: 'The Quick Brown Fox Jumped Over the Lazy Dog'
+    The string printed should be: 'TQBFJOLD'
+
+            Since those are the capital letters in the first string
+"""
+##YOUR SOLUTION HERE
