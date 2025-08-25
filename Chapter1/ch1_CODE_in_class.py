fe5d6570e89d8a9810843754a5a2671c6b40d13e
Yuriy Drubinskiy <ydrubins@yahoo.com>
2025-08-25 09:26:03 -0500

    8/25 update

--- Chapter1/ch1_CODE_in_class.py
+++ Chapter1/ch1_CODE_in_class.py
@@ -3,49 +3,71 @@
 #greet = "How is your day"
 #print(greet)
 
-#print(id(greet))
+# print(id(greet))
 
 ###Slide 15 - Data types
-hello = 'hello'
-print(type(hello))
-
-number = 150
-print(type(number))
-
-a_bool = True
-print(type(a_bool))
-
-hello = number
-print(type(hello))
-
+# hello = 'hello'
+# print(type(hello))
+# #
+# number = 150
+# print(type(number))
+# #
+# a_bool = True
+# print(type(a_bool))
+# #
+# hello = number
+# print(type(hello))
+#
+# score = 3.14
+# print(type(score))
 
 ###Slide 17 - Getting Input
 
-
-
-
-
+## All inputs in Python are strings by default
+# name = input("Enter your name please: ")
+# age = input("Enter your age: ") # Age is a string
+#
+# print(name)
+# print(age)
+# print(type(age))
+#
+#
+# print(age + 1) # This is an error because its trying to combine a string with a none-string
 
 ##Slide 18 Activity
-
-
+# print("Welcome to class")
+#
+# name = input("Enter your name: ")
+# print("Welcome " + name + " to class.")
 
 
 
 
 ####Slide 19 Recasting Input
+# a = input("Enter the first number: ") # THis gives a string
+# b = input("Enter the second number: ") # This gives a string
+#
+# print(a+b)
 
+# c = int(input("Enter the first number: ")) # THis is not an integer
+# d = int(input("Enter the second number: "))
+#
+# print(c + d)
 
-
+# f = float(input("Enter the first number: ")) # THis is now a float
+# g = int(input("Enter the second number: "))
+#
+# print(f + g)
 
 
 
 ###Slide 22 Outputting multiple data
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 
 
-
-pass # Using commas to seperate data
-pass # Using + to concatenate data into one string
+print('Hello',name,'you are',age,'years old.') # Using commas to seperate data
+print('Hello ' + name,' you are ' + str(age),'years old.') # Using + to concatenate data into one string
 pass # Using f-strings
 
 
