a053166572f91191c68889eb3126479c0b5ef576
Yuriy Drubinskiy <ydrubins@yahoo.com>
2025-11-30 14:08:46 -0600

    initial commit

---
+++ Chapter5/Ch5_Dictionary_mini_lesson_in_class.py
@@ -0,0 +1,41 @@
+"""
+A DICTIONARY is a Python data type that links to pieces of data together inside of curly-brackets {}
+    The first piece of data is called the KEY
+    The second piece of data is called the VALUE
+"""
+
+#Define a dictionary
+
+
+
+# We can use the .keys() and the .values() method to view the keys and values separately
+
+
+
+# We can add to a dictionary by specifying the KEY and setting it equal to a value
+
+
+
+    # Create a dictionary entry from user input
+
+
+
+
+#Dictionary are NOT ORDERED meaning we CAN'T use their INDEX to access them or manipulate them
+
+
+
+#Create a dictionary using a loop
+
+
+
+# Dictionaries KEYS have to be UNIQUE, otherwise, the LAST entry will overwrite the previous similar one
+
+
+
+
+# We can perform operations on dictionary keys:
+d = {'a': 13, 'b': 7, 'c' : 12}
+
+    # Add up all the values
+    # Error because can't add strings
